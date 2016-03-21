9f794f08e6df268a1594f9bfe3a2cbbe094f9d29
zolon wilkins <zolonsk8@gmail.com>
2016-03-21 18:00:07 -0500

    building out index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,6 @@
 class ListsController < ApplicationController
 
  def index
-   @responses = Yelp.client.search('Austin', {limit:20, term: 'tacos'}).businesses
+   @responses = Yelp.client.search('Austin', {limit:20, term: 'food'}).businesses
  end
-
-
-
-
 end
