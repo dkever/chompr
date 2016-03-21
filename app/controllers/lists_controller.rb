51987941bb97e23510f85be1d0e8f55f62a60ddd
zolon wilkins <zolonsk8@gmail.com>
2016-03-21 14:57:47 -0500

    added yelp gem

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,12 @@ class ListsController < ApplicationController
  def index
   @lists = List.all
  end
+
+
+
+  def search
+    parameters = { term: params[:term], limit: 16 }
+    render json: Yelp.client.search('San Francisco', parameters)
+  end
+  
 end
---
+++ config/initializers/yelp.rb
@@ -0,0 +1,6 @@
+Yelp.client.configure do |config|
+  config.consumer_key = 'qv2Clt9IMrlc9fW6DlzeSA'
+  config.consumer_secret = 'sezZTXMpAsBokufz7dXgnCyVXnY'
+  config.token = '2zzHBxKoIE73jF4Rfd1laJAgAQJHFl5p'
+  config.token_secret = 'ZT43bNI8W4kDeX3p3Y1UQv5cnAg'
+end
