51f36f312ef0a6f2c9cf736cf9e03836f3d0d087
Kelly Everett <dkever79@yahoo.com>
2016-03-21 18:00:53 -0500

    fixingmergeconflict

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,7 @@
 class ListsController < ApplicationController
 
  def index
-   @responses = Yelp.client.search('Austin', {limit:20, term: 'tacos'}).businesses
+   @responses = Yelp.client.search('Austin', {limit:20, term: 'tacos'}).businesses[4]
 
  end
 
