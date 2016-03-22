f491330e8f254bf432569ce778fdae9ef287efec
zolon wilkins <zolonsk8@gmail.com>
2016-03-22 12:09:33 -0500

    new api up, index started

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,7 +2,7 @@ class ListsController < ApplicationController
 
  def index
    @factual = Factual.new('HnGcIMpIgJsNSBUohResbRnT96Xx8pLjTpH9dVPo', 'YBw3772ijJMk0TMSBbF9FCaN5x89PiEn3G19uPno')
-   @responses = @factual.table("places-us").search("starbucks").filters("$or" => [{"locality" => {"$eq" =>"los angeles"}}, {"locality" => {"$eq" => "santa monica"}}]).page(2, :per => 20).rows
+   @responses = @factual.table("places-us").search("tacos").filters("$or" => [{"locality" => {"$eq" =>"austin"}}]).page(1, :per => 20).rows
  end
 
 end
--- config/initializers/factual.rb
+++ config/initializers/factual.rb
@@ -2,5 +2,4 @@ class Factual
   key = 'HnGcIMpIgJsNSBUohResbRnT96Xx8pLjTpH9dVPo'
   secret = 'YBw3772ijJMk0TMSBbF9FCaN5x89PiEn3G19uPno'
 
-
 end
